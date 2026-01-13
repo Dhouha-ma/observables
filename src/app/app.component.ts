1a5a768449cee7fd4a2d6cfffaeee609c4c474be
Dhouha <dhouha.mansour11@gmail.com>
2026-01-13 12:22:31 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
