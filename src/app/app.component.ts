8edc1bdfeec12785157acb143e764e29baebbc08
Dhouha <dhouha.mansour11@gmail.com>
2026-01-16 13:36:21 +0100

    understanding observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, OnInit, signal } from '@angular/core';
+
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,50 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  customObservable$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+
+        return;
+      }
+
+      console.log('emitting new value');
+      subscriber.next({ message: 'new value' });
+
+      timesExecuted++;
+    }, 2000);
+  });
+
+  constructor(private destroyRef: DestroyRef) {
+    effect(() => {
+      console.log(`clicked button ${this.clickCount()} times.`);
+    });
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    const subscription = interval(1000)
+      .pipe(map((val) => val * 2))
+      .subscribe({
+        next: (val) => console.log(val),
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+
+    this.customObservable$.subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('completed'),
+      error: (error) => console.log(error),
+    });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
   }
 }
